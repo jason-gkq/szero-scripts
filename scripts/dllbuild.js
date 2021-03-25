3523d9d6b79a873050efc1c10850e2432bb3dc00
jason-gkq <guokeqin163@163.com>
2021-03-25 16:25:13 +0800

    删除无用代码

--- scripts/dllbuild.js
+++ scripts/dllbuild.js
@@ -1,14 +1,5 @@
-// @remove-on-eject-begin
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-// @remove-on-eject-end
 'use strict';
 
-const argv = process.argv.slice(2);
 // env 项目自定义环境变量
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
