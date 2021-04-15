1084c6e7da971b4ee940878e17478ba2a2d722c1
jason-gkq <guokeqin163@163.com>
2021-04-15 18:00:36 +0800

    整理提交

--- scripts/build.js
+++ scripts/build.js
@@ -1,35 +1,48 @@
-'use strict';
+"use strict";
 
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
-process.on('unhandledRejection', (err) => {
-	throw err;
+process.on("unhandledRejection", (err) => {
+  throw err;
 });
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const config = require('../config/webpack/webpack.prod');
+const config = require("../config/webpack/webpack.prod");
 
 let compiler = webpack(config);
 
-new webpack.ProgressPlugin().apply(compiler);
+new webpack.ProgressPlugin({
+  activeModules: false,
+  entries: true,
+  handler(percentage, message, ...args) {
+    // e.g. Output each progress message directly to the console:
+    // console.info(percentage, message, ...args);
+  },
+  modules: true,
+  modulesCount: 5000,
+  profile: false,
+  dependencies: true,
+  dependenciesCount: 10000,
+  percentBy: null,
+}).apply(compiler);
 
 compiler.run(function (err, stats) {
-	if (err) {
-		console.error(err.stack || err);
-		if (err.details) {
-			console.error(err.details);
-		}
-		return;
-	}
+  if (err) {
+    console.error(err.stack || err);
+    if (err.details) {
+      console.error(err.details);
+    }
+    return;
+  }
 
-	const info = stats.toJson();
+  const info = stats.toJson();
 
-	if (stats.hasErrors()) {
-		console.error(info.errors);
-	}
+  if (stats.hasErrors()) {
+    console.error(info.errors);
+  }
 
-	if (stats.hasWarnings()) {
-		console.warn(info.warnings);
-	}
+  if (stats.hasWarnings()) {
+    console.warn(info.warnings);
+  }
 });
